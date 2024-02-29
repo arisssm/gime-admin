63279f17987b5af11dd22a9d406dc1149091aaa4
arisssm <arisssmulianto@gmail.com>
2024-02-29 10:06:13 +0700

    update library page

--- controller/LibraryController.js
+++ controller/LibraryController.js
@@ -1,7 +1,29 @@
+const Game = require('../models/Game');
+const User = require('../models/User');
+const Library = require('../models/Library');
+
 module.exports = {
-    index: (req,res) => {
-        res.locals.title = 'Master Data | Library';
-        res.locals.currentPage = 'library';
-        res.render('pages/library');
+    index: async (req, res) => {
+        try {
+            const library = await Library.find().populate('gameId').populate('userId');
+            console.log(library);
+            const game = await Game.find();
+            const usr = await User.find();
+            const user = req.session.user;
+            const alertMsg = req.flash('alertMsg');
+            const alertStatus = req.flash('alertStatus');
+            const alert = {
+                message: alertMsg,
+                status: alertStatus
+            };
+            res.locals.title = 'Master Data | Library';
+            res.locals.currentPage = 'library';
+            res.render('pages/library', { user, library, alert, game, usr });
+        } catch (error) {
+            console.error(error);
+            req.flash('alertMsg', 'An error occurred while processing your request.');
+            req.flash('alertStatus', 'danger');
+            res.redirect('/library');
+        }
     }
 }
