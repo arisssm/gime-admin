95a444b044babd9fe4b3a5af36e11523665981f3
arisssm <arisssmulianto@gmail.com>
2024-03-01 20:00:26 +0700

    change size upload

--- middlewares/multer.js
+++ middlewares/multer.js
@@ -25,7 +25,7 @@ function checkTypeFile(file, cb)
 
 const upload = multer({
     storage:storage,
-    limits: {fileSize:400000},
+    limits: {fileSize:2000000},
     fileFilter: function(req, file, cb)
     {
         checkTypeFile(file, cb);
