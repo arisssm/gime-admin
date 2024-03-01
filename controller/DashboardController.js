c1eb31f90c85b200f694937917449a24b69c6720
arisssm <arisssmulianto@gmail.com>
2024-03-01 20:05:02 +0700

    create dashboard

--- controller/DashboardController.js
+++ controller/DashboardController.js
@@ -1,20 +1,28 @@
 const User = require('../models/User');
+const News = require('../models/News');
+const Game = require('../models/Game');
+const Billing = require('../models/Billing');
+const Cart = require('../models/Cart');
 
 module.exports = {
     index: async (req, res) => {
         try{
+            const user = req.session.user;
+            const countNews = await News.countDocuments();
+            const countGame = await Game.countDocuments();
+            const countBilling = await Billing.countDocuments();
+            const countCart = await Cart.countDocuments();
             const alertMsg = req.flash('alertMsg');
             const alertStatus = req.flash('alertStatus');
             const alert = {
                 message: alertMsg,
                 status: alertStatus
             };
-            const user = req.session.user;
             // req.flash('alertMsg', 'Wecome to Admin, stay strong and goodluck !');
             // req.flash('alertStatus', 'success');
             res.locals.title = 'Dashboard Admin';
             res.locals.currentPage = 'dashboard';
-            res.render('pages/dashboard', {alert, user});
+            res.render('pages/dashboard', {alert, user, countNews, countGame, countBilling, countCart});
         } catch{
             // console.error(error);
             req.flash('alertMsg', error.message);
